20dd435ca3cefb33b5c2003236bd9ea7c6c117dd
Fábio de Carvalho <fdc1994@hotmail.com>
2023-08-27 19:32:32 +0100

    Initial commit and gradle setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,110 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+// Load dependencies
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    val apiKeyPropertiesFile = rootProject.file("apikey.properties")
+    val apiKeyProperties = Properties()
+    apiKeyProperties.load(FileInputStream(apiKeyPropertiesFile))
+
+    namespace = "com.example.marvelheroescompose"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.marvelheroescompose"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+        buildConfigField("String", "MARVEL_KEY", apiKeyProperties.getProperty("MARVEL_PUBLIC_KEY"))
+        buildConfigField("String", "MARVEL_SECRET", apiKeyProperties.getProperty("MARVEL_PRIVATE_KEY"))
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+}
+
+val nav_version = "2.5.2"
+val coil_version = "2.1.0"
+val lifecycle_version = "2.5.1"
+val retrofit_version = "2.9.0"
+val hilt_version = "2.43.2"
+val room_version = "2.4.2"
+
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
+    implementation("androidx.compose.material:material:1.5.0")
+
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    implementation("io.coil-kt:coil-compose:$coil_version")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
+
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+
+    implementation("com.google.dagger:hilt-android:$hilt_version")
+    kapt("com.google.dagger:hilt-compiler:$hilt_version")
+
+    implementation("androidx.room:room-runtime:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+}
